4140966725ffc952f5c4107b6be7acd851647f41
Johanna <johanna.dahlborn@gmail.com>
2021-01-11 20:00:26 +0100

    add
    new

--- src/controller/APIRunner.java
+++ src/controller/APIRunner.java
@@ -3,27 +3,13 @@ package controller;
 import org.json.simple.JSONObject;
 import com.google.gson.Gson;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
 
 import java.util.List;
 
 
 public class APIRunner {
 
-    public static void main(String[] args) throws Exception {
-        port(5000);
-
-        Storage storage = new Storage();
-        storage.setup();
-
-        // A demonstration of how to use code within an endpoint
-        get("/", (req, res) -> {
-            return "Hello world";
-        });
-
-
-    }
+    
 
 
 }

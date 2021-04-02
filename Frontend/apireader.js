3d3bb4dd125c2854c3fac77d4e2880ef6e3ac490
Manna-Manojlovic <manojlovic.manna@gmail.com>
2021-04-02 14:43:30 +0200

    jgjgj

--- Frontend/apireader.js
+++ Frontend/apireader.js
@@ -1,34 +1,31 @@
-var getJSON = function(url, callback) {
-
+var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    xhr.open('get', url, true);
     xhr.responseType = 'json';
-
     xhr.onload = function() {
-
-        var status = xhr.status;
-
-        if (status == 200) {
-            callback(null, xhr.response);
-        } else {
-            callback(status);
-        }
+      var status = xhr.status;
+     if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
     };
-
     xhr.send();
+  });
 };
 
-getJSON('https://api.covid19api.com/dayone/country/sweden/status/confirmed/live
-',  function(err, data) {
+getJSON("https://api.covid19api.com/dayone/country/south-africa/status/confirmed/live").then(function(data) { // Replace <URL> With your URL
+    var jsondata = data.result; //store result in variable
+    var date = jsondata[0].Date;
+    var cases = jsondata[0].Cases;
+    var country = jsondata[0].Country;
+    // Your code here....///
+    ///  Now you can access the json's data using jsondata variable:  //
+    // jsondata[0].year will have the value of year key, jsondata[0].month will have month key and so on.... //
 
-    if (err != null) {
-        console.error(err);
-    } else {
+    consol.
 
-        var text = `Date: ${data.date}
-        Cases: ${data.Cases}
-        Country: ${data.Country}`
-
-        console.log(text);
-    }
+}, function(status) { //error detection....
+  alert('Something went wrong.');
 });
